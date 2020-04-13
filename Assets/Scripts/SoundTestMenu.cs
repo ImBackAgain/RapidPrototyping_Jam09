3e428cc5c8d836f53f31b55c2364040b0a302edf
rhetley <54686199+rhetley@users.noreply.github.com>
2020-04-13 18:07:44 -0400

    Redid sound test scene functionality

---
+++ Assets/Scripts/SoundTestMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SoundTestMenu : MonoBehaviour
+{
+    [Header("Scene Transition Variables")]
+    public string mainMenuSceneName = "";
+
+    // Start()
+    void Start()
+    {
+        
+    }
+
+    // This is called by the menu option within the sound test scene
+    public void ReturnFromSoundTestFunc()
+    {
+        SceneManager.LoadScene(mainMenuSceneName);
+    }
+}
